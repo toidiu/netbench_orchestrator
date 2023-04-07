26b9419c672de0aa5ea3834727d87b87a7357be1
harrisonkaiser <uwaces@amazon.com>
2023-04-07 13:58:41 -0400

    Saving work

--- src/main.rs
+++ src/main.rs
@@ -2,83 +2,299 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-#![allow(clippy::result_large_err)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+use std::{fmt::format, thread::sleep, time::Duration, collections::HashMap};
 
-fn main() {}
-/*
-use aws_config::meta::region::ProvideRegion;
-use aws_sdk_sqs::{Region, PKG_VERSION, Client, Error};
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_ec2 as ec2;
+use aws_sdk_ec2instanceconnect as ec2ic;
+use aws_sdk_iam as iam;
+use aws_sdk_sqs as sqs;
+use aws_sdk_ssm as ssm;
+use aws_types::region::Region;
+use base64::{engine::general_purpose, Engine as _};
+use ec2::types::Filter;
 
-async fn find_first_queue(client: &Client) -> Result<String, Error> {
-    let queues = client.list_queues().send().await?;
-    let queue_urls = queues.queue_urls().unwrap_or_default();
-    Ok(queue_urls
-        .first()
-        .expect("No queues in this account and Region. Create a queue to proceed.")
-        .to_string())
-}
+const ORCH_REGION: &str = "us-west-1";
+const VPC_REGIONS: [&str; 2] = ["us-east-1", "us-west-2"];
 
-// Send a message to a queue.
-async fn send(client: &Client, queue_url: &String, message: &String) -> Result<(), Error> {
-    println!("Sending message to queue with URL: {}", queue_url);
-
-    let rsp = client
-        .send_message()
-        .queue_url(queue_url)
-        .message_body(message)
-        // .message_group_id(&message.group)
-        // .message_deduplication_id("a-unique-id-for-dedup")
-        // If the queue is FIFO, you need to set .message_deduplication_id
-        // or configure the queue for ContentBasedDeduplication.
-        .send()
-        .await?;
-
-    println!("Send message to the queue: {:#?}", rsp);
-
-    Ok(())
-}
-
-// Pump a queue for up to 10 outstanding messages.
-async fn receive(client: &Client, queue_url: &String) -> Result<(), Error> {
-    let rcv_message_output = client.receive_message().queue_url(queue_url).send().await?;
-
-    println!("Messages from queue with url: {}", queue_url);
-
-    for message in rcv_message_output.messages.unwrap_or_default() {
-        println!("Got the message: {:#?}", message);
-    }
-
-    Ok(())
-}
-
-/// Sends a message to and receives the message from a queue in the Region.
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), String> {
+    /*
+     * Overview
+     */
     tracing_subscriber::fmt::init();
-    let queue = None;
-    let verbose = false;
-    let region_provider = Region::new("us-west-2");
 
-    println!();
-    if verbose {
-        println!("SQS client version: {}", PKG_VERSION);
-        println!(
-            "Region:             {}",
-            region_provider.region().await.unwrap().as_ref()
-        );
-        println!();
+    //let region_provider = RegionProviderChain::default_provider().or_else("us-west-2");
+    let orch_provider = Region::new(VPC_REGIONS[0]);
+    let shared_config = aws_config::from_env().region(orch_provider).load().await;
+
+    let ec2_client = ec2::Client::new(&shared_config);
+    //let _sqs_client = sqs::Client::new(&shared_config);
+    //let _iam_client = iam::Client::new(&shared_config);
+    let ec2ic_client = ec2ic::Client::new(&shared_config);
+    let ssm_client = ssm::Client::new(&shared_config);
+
+    // Find the Launch Template for the Netbench Runners
+    let launch_template = get_launch_template(&ec2_client, "NetbenchRunnerTemplate").await?;
+
+    // Find or define the Subnet to Launch the Netbench Runners
+    let subnet_id = get_subnet_ids(&ec2_client, "public-subnet-runners-1").await?;
+
+    /*
+     * Launch instances
+     *
+     * We will define multiple launch templates in CDK for use here.
+     *
+     * For now: Launch 2 instances with the subnet and launch template.
+     */
+    let server_details = InstanceDetails {
+        launch_template: launch_template.clone(),
+        subnet_id: subnet_id.clone(),
+    };
+    let server = launch(&ec2_client, server_details).await?;
+
+    let client_details = InstanceDetails {
+        launch_template: launch_template.clone(),
+        subnet_id: subnet_id.clone(),
+    };
+    let client = launch(&ec2_client, client_details).await?;
+    println!("-----Client----");
+    //println!("{:#?}", client);
+    println!("-----Server----");
+    //println!("{:#?}", server);
+
+    /*
+     * TODO: Wait for instances to be up in a more sophisticated fashion
+     */
+    println!("Waiting for instances to be up:");
+    //sleep(Duration::new(90, 0));
+
+    /*
+     * Setup instances
+     */
+    let client_instance_id = client.instance_id().map(String::from).ok_or(String::from("No client id"))?;
+    let server_instance_id = server.instance_id().map(String::from).ok_or(String::from("No server id"))?;
+    let instance_ids = vec![client_instance_id.clone(), server_instance_id.clone()];
+
+    //let instance_ids: Vec<String> = vec!["i-053182c418b960f50", "i-0fcf5bec9f49422a4"].into_iter().map(String::from).collect();
+    println!("{:?}", instance_ids);
+    let mut parameters = HashMap::new();
+    parameters.insert(String::from("commands"), vec![
+        "echo starting > /home/ec2-user/working",
+        "cd /home/ec2-user",
+        "echo su finished > /home/ec2-user/working",
+        "sudo yum upgrade -y",
+        "sudo yum install cargo git -y",
+        "echo yum finished > /home/ec2-user/working",
+        "runuser -u ec2-user -- git clone https://github.com/aws/s2n-tls.git",
+        "runuser -u ec2-user -- git clone https://github.com/aws/s2n-quic.git",
+        "echo git finished > /home/ec2-user/working",
+        format!("runuser -u ec2-user -- cat <<- \"HEREDOC\"  > /home/ec2-user/request_response.json \n{}\nHEREDOC", include_str!("request_response.json")).as_str(),
+        "echo heredoc finished > /home/ec2-user/working",
+        "cd s2n-quic/netbench"
+        "runuser -u ec2-user -- cargo build --release",
+        "echo build finished > /home/ec2-user/working",
+    ].into_iter().map(String::from).collect());
+    let mut sent_command_result = Err(String::from("NeverCalled Send Command?"));
+    let mut count: u32 = 4;
+    let sent_command = loop {
+        sent_command_result = ssm_client.send_command().set_instance_ids(Some(instance_ids.clone()))
+            .document_name("AWS-RunShellScript").document_version("$LATEST")
+            .set_parameters(Some(parameters.clone()))
+            .send().await.map_err(|x| format!("{:#?}", x));
+        match sent_command_result {
+            Ok(sent_command) => {
+                break sent_command;
+            }
+            Err(error_message) => {
+                if count > 0 {
+                    println!("Error message: {}", error_message);
+                    println!("Trying again, waiting 30 seconds...");
+                    sleep(Duration::new(30, 0));
+                    count -= 1;
+                    continue;
+                } else {
+                    return Err(error_message);
+                }
+            }
+        };
+    };
+
+    println!("{:#?}", sent_command);
+    /*
+     * Retrieve the Secret Key included in launch template
+     */
+    //let private_
+
+    /*
+     * Instance Connect
+     *
+     * Send Public Key to instances
+     */
+    let ssh_public_key = include_str!("id_rsa.pub");
+    let _ssh_private_key = include_str!("id_rsa");
+    // Need instance_id, instance_os_user
+    /*
+    let client_instance_id = client.instance_id().map(String::from);
+    let client_os_user = "ec2-user";
+    let client_az = client
+        .placement()
+        .and_then(ec2::types::Placement::availability_zone)
+        .map(String::from);
+    let server_instance_id = server.instance_id().map(String::from);
+    let server_os_user = "ec2-user";
+    let server_az = server
+        .placement()
+        .and_then(ec2::types::Placement::availability_zone)
+        .map(String::from);
+
+    let client_send_ssh = ec2ic_client
+        .send_ssh_public_key()
+        .set_instance_id(client_instance_id)
+        .instance_os_user(client_os_user)
+        .set_availability_zone(client_az)
+        .ssh_public_key(ssh_public_key)
+        .send()
+        .await
+        .map_err(|r| format!("{:#?}", r))?;
+    println!("Send to the client: {:#?}", client_send_ssh);
+
+
+    if !client_send_ssh.success() {
+        print!("Erroring");
+        return Err("Sending client the ssh key failed!".into());
     }
 
-    let shared_config = aws_config::from_env().region(region_provider).load().await;
-    let client = Client::new(&shared_config);
-    let first_queue_url = find_first_queue(&client).await?;
-    let queue_url = queue.unwrap_or(first_queue_url);
+    let server_send_ssh = ec2ic_client
+        .send_ssh_public_key()
+        .set_instance_id(server_instance_id)
+        .instance_os_user(server_os_user)
+        .set_availability_zone(server_az)
+        .ssh_public_key(ssh_public_key)
+        .send()
+        .await
+        .map_err(|r| format!("{:#?}", r))?
+        .success();
 
-    let message = "hello from my queue".to_owned();
+    if !server_send_ssh {
+        print!("Erroring");
+        return Err("Sending client the ssh key failed!".into());
+    }
+    */
 
-    send(&client, &queue_url, &message).await?;
-    // receive(&client, &queue_url).await?;
+    /*
+     * Orchestrate Test (Open SSH)
+     *
+     *   - sudo yum install cargo git
+     *   - git clone ...s2n-quic.git
+     *   - cd s2n-quic
+     *   - cargo build
+     *   - ...
+     */
 
+    /*
+     * Copy results back
+     */
     Ok(())
 }
-*/
+
+/// Find the Launch Template for the Netbench Runners
+///  This will be used so that we launch the runners in the right
+///  the right security group.
+///  NOTE: if you deploy a new version of the launch template, be
+///        sure to update the default version
+async fn get_launch_template(
+    ec2_client: &ec2::Client,
+    name: &str,
+) -> Result<ec2::types::LaunchTemplateSpecification, String> {
+    let launch_template_name = get_launch_template_name(&ec2_client, name).await?;
+    Ok(
+        ec2::types::builders::LaunchTemplateSpecificationBuilder::default()
+            .launch_template_name(launch_template_name)
+            .version("$Latest")
+            .build(),
+    )
+}
+
+async fn get_launch_template_name(ec2_client: &ec2::Client, name: &str) -> Result<String, String> {
+    let launch_templates: Vec<String> = ec2_client
+        .describe_launch_templates()
+        .launch_template_names(name)
+        .send()
+        .await
+        .map_err(|r| format!("Describe Launch Template Error: {:#?}", r))?
+        .launch_templates()
+        .ok_or("No launch templates?")?
+        .iter()
+        .map(|lt| lt.launch_template_name().unwrap().into())
+        .collect();
+
+    if launch_templates.len() == 1 {
+        Ok(launch_templates.get(0).unwrap().clone())
+    } else {
+        Err("Found more launch templates (or none?)".into())
+    }
+}
+
+// Find or define the Subnet to Launch the Netbench Runners
+//  - Default: Use the one defined by CDK
+// Note: We may need to define more in different regions and AZ
+//      There is some connection between Security Groups and
+//      Subnets such that they have to be "in the same network"
+//       I'm unclear here.
+async fn get_subnet_ids(ec2_client: &ec2::Client, subnet_name: &str) -> Result<String, String> {
+    let describe_subnet_output = ec2_client
+        .describe_subnets()
+        .filters(
+            ec2::types::Filter::builder()
+                .name("tag:aws-cdk:subnet-name")
+                .values(subnet_name)
+                .build(),
+        )
+        .send()
+        .await
+        .map_err(|e| format!("Couldn't describe subnets: {:#?}", e))?;
+    assert_eq!(
+        describe_subnet_output.subnets().expect("No subnets?").len(),
+        1
+    );
+    let subnet_id = describe_subnet_output.subnets().unwrap()[0]
+        .subnet_id()
+        .ok_or::<String>("Couldn't find subnet".into())?;
+    Ok(subnet_id.into())
+}
+
+/*
+ * Launch instance
+ *
+ * This function launches a single instance. It is configurable using
+ * this struct.
+ */
+struct InstanceDetails {
+    launch_template: ec2::types::LaunchTemplateSpecification,
+    subnet_id: String,
+}
+async fn launch(
+    ec2_client: &ec2::Client,
+    instance_details: InstanceDetails,
+) -> Result<ec2::types::Instance, String> {
+    let run_result = ec2_client
+        .run_instances()
+        .launch_template(instance_details.launch_template)
+        .min_count(1)
+        .max_count(1)
+        .dry_run(false)
+        .subnet_id(instance_details.subnet_id)
+        .send()
+        .await
+        .map_err(|r| format!("{:#?}", r))?;
+    let instances = run_result
+        .instances()
+        .ok_or::<String>("Couldn't find instances in run result".into())?;
+    Ok(instances
+        .get(0)
+        .ok_or(String::from("Didn't launch an instance?"))?
+        .clone())
+}
--- src/runner.rs
+++ src/runner.rs
@@ -4,18 +4,24 @@
  */
 #![allow(clippy::result_large_err)]
 #![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+use std::fmt::format;
+
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_ec2 as ec2;
+use aws_sdk_ec2instanceconnect as ec2ic;
 use aws_sdk_iam as iam;
 use aws_sdk_sqs as sqs;
 use base64::{engine::general_purpose, Engine as _};
+use ec2::types::Filter;
 
 #[derive(Debug)]
 enum RunnerError {
     SQS(sqs::Error),
     EC2(ec2::Error),
     IAM(iam::Error),
-    Str(&'static str),
+    Str(String),
     Unit,
 }
 
@@ -44,6 +50,7 @@ impl From<()> for RunnerError {
 }
 async fn find_first_queue(client: &sqs::Client) -> Result<String, sqs::Error> {
     let queues = client.list_queues().send().await?;
+    println!("{:#?}", queues);
     let queue_urls = queues.queue_urls().unwrap_or_default();
     assert!(
         queue_urls.len() <= 1,
@@ -55,6 +62,27 @@ async fn find_first_queue(client: &sqs::Client) -> Result<String, sqs::Error> {
         .to_string())
 }
 
+async fn get_security_groups(client: &ec2::Client) -> Option<String> {
+    let sec_groups = client
+        .describe_security_groups()
+        .filters(
+            ec2::types::Filter::builder()
+                .name("description")
+                .values("security group for the netbench runners")
+                .build(),
+        )
+        .send()
+        .await
+        .expect("Hello");
+    sec_groups
+        .security_groups()
+        .unwrap()
+        .get(0)
+        .expect("Couldn't find a sec group? are you authenticated?")
+        .group_name()
+        .map(|c| c.into())
+}
+
 ///
 /// ```rust
 ///     async fn launch(client: &aws_skd_ec2::Client, ) -> Result<Vec<String>, RunnerError>
@@ -64,18 +92,21 @@ async fn find_first_queue(client: &sqs::Client) -> Result<String, sqs::Error> {
 /// 2) Runner gets info -> is ready for the run!
 /// 3) Runner triggers the run
 ///
-async fn launch(ec2_client: &ec2::Client, queue_url: &String) -> Result<Vec<String>, RunnerError> {
-    let script = general_purpose::STANDARD_NO_PAD.encode(format!("aws sqs send-message --queue-url {queue_url} --message-body \"`ec2-metadata -i`\" --region us-west-2"));
+async fn launch(
+    ec2_client: &ec2::Client,
+    launch_template: ec2::types::LaunchTemplateSpecification,
+    subnet_id: impl Into<String>,
+) -> Result<Vec<ec2::types::Instance>, RunnerError> {
     match ec2_client
         .run_instances()
+        .launch_template(launch_template)
         .min_count(1)
         .max_count(1)
         .dry_run(false)
-        .image_id("ami-0efa651876de2a5ce")
-        .user_data(script)
-        .iam_instance_profile(
-            ec2::types::IamInstanceProfileSpecification::builder()
-                .name("NetbenchRunner")
+        .subnet_id(subnet_id.into())
+        .cpu_options(
+            ec2::types::CpuOptionsRequest::builder()
+                .core_count(8)
                 .build(),
         )
         .send()
@@ -85,19 +116,7 @@ async fn launch(ec2_client: &ec2::Client, queue_url: &String) -> Result<Vec<Stri
             println!("Error: {:#?}", err);
             Err(())?
         }
-        Ok(rio) => {
-            println!("Ok: {:#?}", rio);
-            Ok(rio
-                .instances()
-                .iter()
-                .flat_map(|instances| {
-                    instances
-                        .iter()
-                        .filter_map(|instance| instance.instance_id())
-                })
-                .map(|s| s.into())
-                .collect())
-        }
+        Ok(rio) => Ok(rio.instances().unwrap().iter().map(|i| i.clone()).collect()),
     }
 }
 
@@ -166,16 +185,36 @@ async fn receive(client: &sqs::Client, queue_url: &String) -> Result<(), sqs::Er
     Ok(())
 }
 
-async fn new_instance_profile(client: &iam::Client) -> Result<iam::types::Role, RunnerError> {
-    client
+async fn get_instance_profile(client: &iam::Client) -> Result<String, RunnerError> {
+    /*let role = client
         .get_role()
-        .role_name("*")
+        .role_name("NetbenchRunner")
         .send()
         .await
-        .map_err(|_| RunnerError::Str("No role found in region"))?
+        .map_err(|a| RunnerError::Str(format!("{:#?}", a)))?
         .role()
         .cloned()
-        .ok_or(RunnerError::Unit)
+        .expect("role failed");
+    */
+
+    let instance_profile = client
+        .get_instance_profile()
+        .instance_profile_name("NetbenchRunnerInstanceProfile")
+        .send()
+        .await
+        .map_err(|a| RunnerError::Str(format!("{:#?}", a)))?
+        .instance_profile()
+        .expect("ip failed")
+        .clone();
+    /*
+    client.add_role_to_instance_profile()
+        .instance_profile_name("NetbenchRunnerInstanceProfile1").role_name("NetbenchRunner").send().await.expect("Couldn't add role to instance profile?");
+    */
+
+    instance_profile
+        .arn()
+        .map(str::to_string)
+        .ok_or(RunnerError::Str("No instance profile found".into()))
 }
 
 /// Sends a message to and receives the message from a queue in the Region.
@@ -184,16 +223,88 @@ async fn main() -> Result<(), RunnerError> {
     tracing_subscriber::fmt::init();
 
     let region_provider = RegionProviderChain::default_provider().or_else("us-west-2");
+    // print!("{:#?}", region_provider);
     let shared_config = aws_config::from_env().region(region_provider).load().await;
 
     let ec2_client = ec2::Client::new(&shared_config);
     let sqs_client = sqs::Client::new(&shared_config);
     let iam_client = iam::Client::new(&shared_config);
+    let ec2ic_client = ec2ic::Client::new(&shared_config);
 
-    let role = new_instance_profile(&iam_client).await?;
-    println!("got role: {:#?}", role);
+    // 1. Get the launch template
+    let launch_templates: Vec<String> = ec2_client
+        .describe_launch_templates()
+        .launch_template_names("NetbenchRunnerTemplate")
+        .send()
+        .await
+        .expect("No launch template?")
+        .launch_templates()
+        .expect("No launch templates? 2")
+        .iter()
+        .map(|lt| lt.launch_template_name().unwrap().into())
+        .collect();
 
+    assert_eq!(launch_templates.len(), 1);
+    let launch_template = launch_templates.get(0).unwrap();
+    print!("Got a launch template: {}", launch_template);
+
+    // 2. Get SQS queue. -- might not need this in the end -- move to launch template?
     //let queue_url = find_first_queue(&sqs_client).await?;
+
+    // 3. Get VPC
+    let x = ec2_client
+        .describe_subnets()
+        .filters(
+            /* .values("public-subnet-runners-1") */
+            ec2::types::Filter::builder()
+                .name("tag:aws-cdk:subnet-name")
+                .values("public-subnet-runners-1")
+                .build(),
+        )
+        .send()
+        .await
+        .expect("error");
+    assert_eq!(x.subnets().expect("None?").len(), 1);
+    let subnet_id = x.subnets().unwrap()[0].subnet_id().expect("No vpc id?");
+    println!("Subnets\n{:#?}", subnet_id);
+
+    // 3. Launch w/ Launch Template
+    let instances = launch(
+        &ec2_client,
+        ec2::types::builders::LaunchTemplateSpecificationBuilder::default()
+            .launch_template_name(launch_template)
+            .build(),
+        subnet_id,
+    )
+    .await?;
+
+    println!("{:#?}", instances);
+
+    let instance_ids: Vec<String> = instances
+        .iter()
+        .map(|instance| instance.instance_id().unwrap().into())
+        .collect();
+
+    let public_key = include_str!("id_rsa.pub");
+    let private_key = include_str!("id_rsa");
+
+    for id in instance_ids.iter() {
+        loop {
+            let result = ec2ic_client
+                .send_ssh_public_key()
+                .instance_os_user("ec2-user")
+                .ssh_public_key(public_key)
+                .instance_id(id)
+                .send()
+                .await;
+            if result.is_ok() {
+                break;
+            } else {
+            }
+        }
+    }
+    //launch(&ec2_client, &queue_url, instance_profile).await?;
+
     //println!("{}", queue_url);
     //launch(&ec2_client, &queue_url).await?;
     //receive(&client, &queue_url).await?;
