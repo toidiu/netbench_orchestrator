13c21bbd9f124ecabc791864984b6a25729c0afc
Apoorv Kothari <apoorv@toidiu.com>
2023-11-03 15:49:36 -0700

    wip compile state_action

--- src/russula/state_action.rs
+++ src/russula/state_action.rs
@@ -1,100 +1,110 @@
-// use super::protocol::StateApi;
-// use crate::russula::Protocol;
-// use crate::russula::RussulaResult;
-// use crate::russula::TransitionStep;
-// use async_trait::async_trait;
-// use bytes::Bytes;
-// use core::fmt::Debug;
-// use std::net::SocketAddr;
-// use tokio::net::TcpStream;
+#![allow(unused_imports)]
+use super::protocol::StateApi;
+use crate::russula::Protocol;
+use crate::russula::RussulaResult;
+use crate::russula::TransitionStep;
+use async_trait::async_trait;
+use bytes::Bytes;
+use core::fmt::Debug;
+use std::net::SocketAddr;
+use tokio::net::TcpStream;
 
-// pub trait StateAction: Debug {
-//     fn run(&self) {}
-// }
-// #[derive(Clone, Debug)]
-// pub struct NotifyPeer {}
-// impl StateAction for NotifyPeer {}
-// #[derive(Clone, Debug)]
-// pub struct WaitPeerState {}
-// pub struct RunSome {}
-// pub struct Sleep {}
-// impl StateAction for WaitPeerState {}
+pub trait StateAction: Debug {
+    fn run(&self) {}
+}
+#[derive(Clone, Debug)]
+pub struct NotifyPeer {}
+impl StateAction for NotifyPeer {}
+#[derive(Clone, Debug)]
+pub struct WaitPeerState {}
+pub struct RunSome {}
+pub struct Sleep {}
+impl StateAction for WaitPeerState {}
 
-// #[derive(Debug)]
-// pub enum MyProtocolState {
-//     CheckPeer(Vec<Box<dyn StateAction>>),
-//     Bla(Vec<Box<dyn StateAction>>),
-// }
+#[derive(Debug)]
+pub enum MyProtocolState {
+    CheckPeer(Vec<Box<dyn StateAction>>),
+    Bla(Vec<Box<dyn StateAction>>),
+}
 
-// // impl Clone for MyProtocolState {
-// //     fn clone(&self) -> Self {
-// //         match self {
-// //             Self::CheckPeer(arg0) => Self::CheckPeer(arg0.clone().to_vec()),
-// //         }
-// //     }
-// // }
+unsafe impl Send for MyProtocolState {}
 
-// #[async_trait]
-// impl StateApi for MyProtocolState {
-//     async fn run(&mut self, stream: &TcpStream) {}
-//     fn eq(&self, other: &Self) -> bool {
-//         todo!()
-//     }
-//     fn transition_step(&self) -> TransitionStep {
-//         todo!()
-//     }
-//     fn next(&mut self) {
-//         todo!()
-//     }
-//     fn process_msg(&mut self, msg: Bytes) {
-//         todo!()
-//     }
-//     fn as_bytes(&self) -> &'static [u8] {
-//         todo!()
-//     }
-//     fn from_bytes(bytes: &[u8]) -> RussulaResult<Self> {
-//         todo!()
-//     }
-// }
+impl Clone for MyProtocolState {
+    fn clone(&self) -> Self {
+        todo!()
+        // match self {
+        //     Self::CheckPeer(arg0) => Self::CheckPeer(arg0.clone().to_vec()),
+        // }
+    }
+}
 
-// impl CoordServerState {
-//     fn run(&self) {
-//         let state =
-//             CoordServerState::CheckPeer(vec![Box::new(NotifyPeer {}), Box::new(WaitPeerState {})]);
-//         match state {
-//             CoordServerState::CheckPeer(v) => v.iter().for_each(|s| {
-//                 s.run();
-//             }),
-//         }
-//     }
-// }
+#[async_trait]
+impl StateApi for MyProtocolState {
+    async fn run(&mut self, stream: &TcpStream) {}
+    fn eq(&self, other: &Self) -> bool {
+        todo!()
+    }
+    fn transition_step(&self) -> TransitionStep {
+        todo!()
+    }
+    fn next(&mut self) {
+        todo!()
+    }
+    fn process_msg(&mut self, msg: Bytes) {
+        todo!()
+    }
+    fn as_bytes(&self) -> &'static [u8] {
+        todo!()
+    }
+    fn from_bytes(bytes: &[u8]) -> RussulaResult<Self> {
+        todo!()
+    }
+}
 
-// // #[derive(Clone, Debug)]
-// // struct MyProtocol {
-// //     state: MyProtocolState,
-// // }
+impl MyProtocolState {
+    fn run(&self) {
+        let state =
+            MyProtocolState::CheckPeer(vec![Box::new(NotifyPeer {}), Box::new(WaitPeerState {})]);
+        match state {
+            MyProtocolState::CheckPeer(v) => v.iter().for_each(|s| {
+                s.run();
+            }),
+            MyProtocolState::Bla(v) => v.iter().for_each(|s| {
+                s.run();
+            }),
+        }
+    }
+}
 
-// // #[async_trait]
-// // impl Protocol for MyProtocol {
-// //     type State = MyProtocolState;
+#[derive(Clone, Debug)]
+struct MyProtocol {
+    state: MyProtocolState,
+}
 
-// //     async fn connect(&self, addr: &SocketAddr) -> RussulaResult<TcpStream> {
-// //         todo!()
-// //     }
-// //     async fn run_till_ready(&mut self, stream: &TcpStream) -> RussulaResult<()> {
-// //         todo!()
-// //     }
-// //     async fn run_till_done(&mut self, stream: &TcpStream) -> RussulaResult<()> {
-// //         todo!()
-// //     }
-// //     async fn run_till_state(
-// //         &mut self,
-// //         stream: &TcpStream,
-// //         state: Self::State,
-// //     ) -> RussulaResult<()> {
-// //         todo!()
-// //     }
-// //     fn state(&self) -> Self::State {
-// //         todo!()
-// //     }
-// // }
+unsafe impl Sync for MyProtocol {}
+unsafe impl Send for MyProtocol {}
+
+#[async_trait]
+impl Protocol for MyProtocol {
+    type State = MyProtocolState;
+
+    async fn connect(&self, addr: &SocketAddr) -> RussulaResult<TcpStream> {
+        todo!()
+    }
+    async fn run_till_ready(&mut self, stream: &TcpStream) -> RussulaResult<()> {
+        todo!()
+    }
+    async fn run_till_done(&mut self, stream: &TcpStream) -> RussulaResult<()> {
+        todo!()
+    }
+    async fn run_till_state(
+        &mut self,
+        stream: &TcpStream,
+        state: Self::State,
+    ) -> RussulaResult<()> {
+        todo!()
+    }
+    fn state(&self) -> &Self::State {
+        todo!()
+    }
+}
