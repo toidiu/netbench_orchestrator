89beb1a1b74dd12a93f412282a6a53b1d7f0ae8f
Apoorv Kothari <apoorv@toidiu.com>
2023-12-15 14:03:13 -0800

    rm manual wait steps

--- src/ssm_utils/client.rs
+++ src/ssm_utils/client.rs
@@ -11,18 +11,19 @@ pub async fn run_client_russula(
 ) -> SendCommandOutput {
     send_command(vec![Step::BuildRussula], Step::RunRussula, "client", "run_client_russula", ssm_client, instance_ids, vec![
         // russula START
-        "cd /home/ec2-user",
-        "until [ -f russula_build_fin ]; do sleep 5; done",
-        "sleep 5",
-        "touch russula_start----------",
+        // "cd /home/ec2-user",
+        // "until [ -f russula_build_fin ]; do sleep 5; done",
+        // "sleep 5",
+        // "touch russula_start----------",
+
         // format!("runuser -u ec2-user -- git clone --branch {} {}", STATE.russula_branch, STATE.russula_repo).as_str(),
         "cd netbench_orchestrator",
         // "runuser -u ec2-user -- cargo build",
         format!("env RUST_LOG=debug ./target/debug/russula --protocol NetbenchClientWorker --port {}", STATE.russula_port).as_str(),
         // russula END
         // log
-        "cd /home/ec2-user",
-        "touch russula_run_fin",
+        // "cd /home/ec2-user",
+        // "touch russula_run_fin",
         "exit 0"
     ].into_iter().map(String::from).collect()).await.expect("Timed out")
 }
@@ -34,10 +35,11 @@ pub async fn run_client_netbench(
     unique_id: &str,
 ) -> SendCommandOutput {
     send_command(vec![Step::BuildNetbench], Step::RunNetbench, "client", "run_client_netbench", ssm_client, instance_ids, vec![
-        "cd /home/ec2-user",
-        "until [ -f build_netbench_fin ]; do sleep 5; done",
-        "sleep 5",
-        "touch netbench_run_start----------",
+        // "cd /home/ec2-user",
+        // "until [ -f build_netbench_fin ]; do sleep 5; done",
+        // "sleep 5",
+        // "touch netbench_run_start----------",
+
         // format!("runuser -u ec2-user -- git clone --branch {} {}", STATE.netbench_branch, STATE.netbench_repo).as_str(),
         // format!("runuser -u ec2-user -- echo git finished > /home/ec2-user/index.html && aws s3 cp /home/ec2-user/index.html {}/client-step-4", STATE.s3_path(unique_id)).as_str(),
         // format!("runuser -u ec2-user -- aws s3 cp s3://{}/{}/request_response.json /home/ec2-user/request_response.json", STATE.s3_log_bucket, STATE.s3_resource_folder).as_str(),
@@ -58,8 +60,8 @@ pub async fn run_client_netbench(
         // FIXME move to start of ssm commands
         "shutdown -h +60",
         // log
-        "cd /home/ec2-user",
-        "touch netbench_run_fin",
+        // "cd /home/ec2-user",
+        // "touch netbench_run_fin",
         "exit 0"
     ].into_iter().map(String::from).collect()).await.expect("Timed out")
 }
--- src/ssm_utils/common.rs
+++ src/ssm_utils/common.rs
@@ -12,16 +12,17 @@ pub async fn configure_hosts(
     unique_id: &str,
 ) -> SendCommandOutput {
     send_command(vec![], Step::Configure, host_group, "configure_host",ssm_client, instance_ids, vec![
-        "cd /home/ec2-user",
-        "touch config_start----------",
+        // "cd /home/ec2-user",
+        // "touch config_start----------",
+        
         format!("runuser -u ec2-user -- echo ec2 up > /home/ec2-user/index.html && aws s3 cp /home/ec2-user/index.html {}/{}-step-1", STATE.s3_path(unique_id), host_group).as_str(),
         "yum upgrade -y",
         format!("runuser -u ec2-user -- echo yum upgrade finished > /home/ec2-user/index.html && aws s3 cp /home/ec2-user/index.html {}/{}-step-2", STATE.s3_path(unique_id), host_group).as_str(),
         format!("timeout 5m bash -c 'until yum install cmake cargo git perl openssl-devel bpftrace perf tree -y; do sleep 10; done' || (echo yum failed > /home/ec2-user/index.html; aws s3 cp /home/ec2-user/index.html {}/server-step-3; exit 1)", STATE.s3_path(unique_id)).as_str(),
         format!("echo yum finished > /home/ec2-user/index.html && aws s3 cp /home/ec2-user/index.html {}/{}-step-3", STATE.s3_path(unique_id), host_group).as_str(),
         // log
-        "cd /home/ec2-user",
-        "touch config_fin",
+        // "cd /home/ec2-user",
+        // "touch config_fin",
         "exit 0"
     ].into_iter().map(String::from).collect()).await.expect("Timed out")
 }
@@ -40,10 +41,11 @@ pub async fn build_russula(
         instance_ids,
         vec![
             // russula START
-            "cd /home/ec2-user",
-            "until [ -f config_fin ]; do sleep 5; done",
-            "sleep 5",
-            "touch russula_build_start----------",
+            // "cd /home/ec2-user",
+            // "until [ -f config_fin ]; do sleep 5; done",
+            // "sleep 5",
+            // "touch russula_build_start----------",
+
             format!(
                 "runuser -u ec2-user -- git clone --branch {} {}",
                 STATE.russula_branch, STATE.russula_repo
@@ -53,8 +55,8 @@ pub async fn build_russula(
             "runuser -u ec2-user -- cargo build",
             // russula END
             // log
-            "cd /home/ec2-user",
-            "touch russula_build_fin",
+            // "cd /home/ec2-user",
+            // "touch russula_build_fin",
             "exit 0",
         ]
         .into_iter()
@@ -74,10 +76,11 @@ pub async fn build_netbench(
     send_command(
         vec![Step::Configure],
         Step::BuildNetbench,host_group, "run_netbench", ssm_client, instance_ids, vec![
-        "cd /home/ec2-user",
-        "until [ -f russula_run_fin ]; do sleep 5; done",
-        "sleep 5",
-        "touch build_netbench_start----------",
+        // "cd /home/ec2-user",
+        // "until [ -f russula_run_fin ]; do sleep 5; done",
+        // "sleep 5",
+        // "touch build_netbench_start----------",
+
         format!("runuser -u ec2-user -- git clone --branch {} {}", STATE.netbench_branch, STATE.netbench_repo).as_str(),
         format!("runuser -u ec2-user -- echo clone_netbench > /home/ec2-user/index.html && aws s3 cp /home/ec2-user/index.html {}/{}-step-4", STATE.s3_path(unique_id), host_group).as_str(),
         format!("runuser -u ec2-user -- aws s3 cp s3://{}/{}/request_response.json /home/ec2-user/request_response.json", STATE.s3_log_bucket, STATE.s3_resource_folder).as_str(),
@@ -88,8 +91,8 @@ pub async fn build_netbench(
         "runuser -u ec2-user -- cp /home/ec2-user/request_response.json target/netbench/request_response.json",
         format!("runuser -u ec2-user -- echo cargo build finished > /home/ec2-user/index.html && aws s3 cp /home/ec2-user/index.html {}/{}-step-6", STATE.s3_path(unique_id), host_group).as_str(),
         // log
-        "cd /home/ec2-user",
-        "touch build_netbench_fin",
+        // "cd /home/ec2-user",
+        // "touch build_netbench_fin",
         "exit 0"
     ].into_iter().map(String::from).collect()).await.expect("Timed out")
 }
