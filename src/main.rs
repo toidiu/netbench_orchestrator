c365b52958427f4803ff606f31f04864b988597d
Apoorv Kothari <apoorv@toidiu.com>
2023-12-16 02:03:59 -0800

    f

--- src/main.rs
+++ src/main.rs
@@ -133,18 +133,26 @@ async fn main() -> OrchResult<()> {
     .await?;
 
     // configure and build
-    let mut build_cmds =
-        ssm_utils::common::config_build_cmds("server", &ssm_client, server_ids.clone(), &unique_id)
-            .await;
-    let client_build_cmds =
-        ssm_utils::common::config_build_cmds("client", &ssm_client, client_ids.clone(), &unique_id)
-            .await;
-    build_cmds.extend(client_build_cmds);
-    ssm_utils::common::poll_cmds("client_server", &ssm_client, build_cmds).await;
+    {
+        let mut build_cmds = ssm_utils::common::config_build_cmds(
+            "server",
+            &ssm_client,
+            server_ids.clone(),
+            &unique_id,
+        )
+        .await;
+        let client_build_cmds = ssm_utils::common::config_build_cmds(
+            "client",
+            &ssm_client,
+            client_ids.clone(),
+            &unique_id,
+        )
+        .await;
+        build_cmds.extend(client_build_cmds);
+        ssm_utils::common::poll_cmds("client_server", &ssm_client, build_cmds).await;
+    }
 
     // run russula
-
-    // client russula
     {
         // client run commands
         let run_client_russula =
@@ -171,36 +179,46 @@ async fn main() -> OrchResult<()> {
     }
 
     // run netbench
-    let run_server_netbench =
-        ssm_utils::server::run_netbench(&ssm_client, server_ids.clone(), &client.ip, &unique_id)
-            .await;
-    let run_client_netbench =
-        ssm_utils::client::run_netbench(&ssm_client, client_ids.clone(), &server.ip, &unique_id)
-            .await;
-    loop {
-        // client netbench
-        let client_result = poll_ssm_results(
-            "client",
+    {
+        let run_server_netbench = ssm_utils::server::run_netbench(
             &ssm_client,
-            run_client_netbench.command().unwrap().command_id().unwrap(),
+            server_ids.clone(),
+            &client.ip,
+            &unique_id,
         )
-        .await
-        .unwrap();
-        info!("Client Finished!: Successful: {:?}", client_result);
-        // server
-        let server_result = poll_ssm_results(
-            "server",
+        .await;
+        let run_client_netbench = ssm_utils::client::run_netbench(
             &ssm_client,
-            run_server_netbench.command().unwrap().command_id().unwrap(),
+            client_ids.clone(),
+            &server.ip,
+            &unique_id,
         )
-        .await
-        .unwrap();
-        info!("Server Finished!: Successful: {:?}", server_result);
+        .await;
+        loop {
+            // client netbench
+            let client_result = poll_ssm_results(
+                "client",
+                &ssm_client,
+                run_client_netbench.command().unwrap().command_id().unwrap(),
+            )
+            .await
+            .unwrap();
+            info!("Client Finished!: Successful: {:?}", client_result);
+            // server
+            let server_result = poll_ssm_results(
+                "server",
+                &ssm_client,
+                run_server_netbench.command().unwrap().command_id().unwrap(),
+            )
+            .await
+            .unwrap();
+            info!("Server Finished!: Successful: {:?}", server_result);
 
-        if client_result.is_ready() && server_result.is_ready() {
-            break;
+            if client_result.is_ready() && server_result.is_ready() {
+                break;
+            }
+            tokio::time::sleep(Duration::from_secs(10)).await;
         }
-        tokio::time::sleep(Duration::from_secs(10)).await;
     }
 
     // Copy results back
