3d8bb2dd95bc9daae76a5b9e1b847ec9506bfaee
Apoorv Kothari <apoorv@toidiu.com>
2023-12-16 03:04:39 -0800

    v1.0.29

--- src/state.rs
+++ src/state.rs
@@ -5,7 +5,7 @@ use crate::ec2_utils::EndpointType;
 use core::time::Duration;
 
 pub const STATE: State = State {
-    version: "v1.0.28",
+    version: "v1.0.29",
 
     // netbench
     netbench_repo: "https://github.com/aws/s2n-quic.git",
