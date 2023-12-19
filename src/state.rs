7085125a614d0a951cd9d59980a24470a2f2d0a3
apoorvko <apoorvko@amazon.com>
2023-12-18 23:16:27 -0800

    v1.0.31

--- src/state.rs
+++ src/state.rs
@@ -4,7 +4,7 @@
 use crate::ec2_utils::EndpointType;
 
 pub const STATE: State = State {
-    version: "v1.0.30",
+    version: "v1.0.31",
 
     // netbench
     netbench_repo: "https://github.com/aws/s2n-quic.git",
