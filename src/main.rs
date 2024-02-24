d586d4a7153cd50481c21fc76a93688cc78c0851
apoorvko <apoorvko@amazon.com>
2024-02-23 17:56:24 -0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -50,5 +50,5 @@ async fn main() -> OrchResult<()> {
     let aws_config = aws_config::from_env().region(region).load().await;
     let config = cli.check_requirements(&aws_config).await?;
 
-    orchestrator::run(unique_id, &config, &aws_config, RunMode::Full).await
+    orchestrator::run(unique_id, &config, &aws_config, RunMode::TestInfra).await
 }
